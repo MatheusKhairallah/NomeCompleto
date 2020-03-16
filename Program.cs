5083630da09877512a491df312ec5bfa870cdc2a
matheus <playermaster10like@gmail.com>
2020-03-16 18:35:51 -0300

    programa c#

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string nome=Console.ReadLine();
+
+            Console.ReadKey();
+
+            Console.Write("Agora digite seu sobrenome: ");
+            string sobrenome=Console.ReadLine();
+            string sobrenome2=sobrenome.ToUpper();
+
+            Console.Clear();
+            
+            string mensagem="Nome completo: " +nome+ " " +sobrenome+ ".";
+
+            Console.WriteLine(mensagem);
+
+            string mensagem2="Nome de catálogo: " +sobrenome2+ ", " +nome+ ".";
+
+            Console.WriteLine(mensagem2);
+        }
+    }
+}
